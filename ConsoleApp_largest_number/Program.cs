52838c33377761eadb3a21528330ba4fbf315974
niles <niles@LAPTOP-3K24CQ1R>
2021-08-29 22:49:23 +0530

    Add project files.

---
+++ ConsoleApp_largest_number/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace ConsoleApp_largest_number
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num1,num2,num3;
+            Console.WriteLine("Find the largest of three number :\n");
+            Console.Write("Enter 1st number :");
+            num1 = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Enter 2nd number :");
+            num2 = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Enter 3rd number :");
+            num3 = Convert.ToInt32(Console.ReadLine());
+
+            if(num1>num2)
+            {
+                if(num1>num3)
+                {
+                    Console.WriteLine("The first number is the greater than three");
+                }
+                else
+                {
+                    Console.WriteLine("The thrid number is the greater than three");
+
+                }
+            }
+            else if(num2>num3)
+            {
+                Console.WriteLine("The second number is the greater than three");
+
+            }
+            else
+            {
+                Console.WriteLine("The third number is the greater than three");
+
+            }
+
+        }
+    }
+}
